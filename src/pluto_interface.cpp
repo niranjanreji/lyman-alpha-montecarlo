32647cdda9170061b67c1e92ba6f5e07ea4649e9
Niranjan <niranjanrejii@gmail.com>
2026-01-02 12:51:25 +0530

    Update pluto_interface.cpp

--- src/pluto_interface.cpp
+++ src/pluto_interface.cpp
@@ -2,17 +2,19 @@
 
 extern "C" {
     void RadiativeTransfer(
+        double dt, 
         const double* rho,
         const double* vr, 
         const double* r,
         int n,
         double* out_force
-    ) {
+    ) 
+    {
         const string fname = "../input/grid.h5";
 
         Grid* grid = load_grid(fname);
         static Photons* p = new Photons();
         
-        monte_carlo(*p, *grid, 1e20, 100, true);
+        monte_carlo(*p, *grid, dt, 100, true);
     }
 }
